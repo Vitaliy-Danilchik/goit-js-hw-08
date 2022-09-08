491c9daf5c416a48de8a367ccf10a34e415ae8df
Vitaliy-Danilchik <danilchikvvv@gmail.com>
2022-09-08 23:07:52 +0300

    goit-js-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML("afterbegin", createGalleryCards(galleryItems));
+
+
+function createGalleryCards(galleryItems) {
+	return galleryItems.map(({ preview, original, description }) => {
+		return `<a class="gallery__item" href="${original}"}><img class="gallery__image" src="${preview}" alt="${description}"/>
+	 </a>` }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
